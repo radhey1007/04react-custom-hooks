03079eba0bd2ad7a5426110d6dabffeb4c9304e6
radhey1007 <radheysv78@gmail.com>
2022-02-01 14:51:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  // destructuring and aliasing (whatever the name u can)
+  const {isLoading , error , sendRequest:fetchTasks} = useHttp();
+  const requestConfig = { 
+    url: 'https://react-http-8c01d-default-rtdb.firebaseio.com/tasks.json'
+  }  
+  useEffect(() => {
+    const transformedTasks = (data) => {
+      const loadedTasks = [];
+      for (const taskKey in data) {
+        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks(requestConfig , transformedTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
